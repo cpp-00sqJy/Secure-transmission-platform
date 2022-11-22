7d29be65e1b376b738ea048126cf377b18d6d6f6
niuzi <niuzi@henda.com>
2022-11-22 22:15:27 +0800

    Aes 共享内存 Rsa

---
+++ Interface/nzInterface.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+#include <string>
+
+
+
+using namespace std;
+
+class nzInterface
+{
+public:
+
+	nzInterface(string json);
+
+	~nzInterface();
+
+	string encData(const string& data);
+
+	string decData(const string& data);
+
+
+
+private:
+	string key;
+	int maxNode;
+	string m_key;
+	int status;
+	int m_SecKeyID;
+
+
+};
+
