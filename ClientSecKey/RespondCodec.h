5e035fdd328b7ad736838914824f2eb9542dd5be
niuzi <niuzi@henda.com>
2022-11-23 13:19:47 +0800

    .

---
+++ ClientSecKey/RespondCodec.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "Codec.h"
+
+struct Respondinfo
+{
+    int status;
+    int seckeyID;
+    string serverID;
+    string clientID;
+    string data;
+};
+
+class RespondCodec :
+    public Codec
+{
+public:
+    RespondCodec();
+    RespondCodec(string encstr);
+    RespondCodec(Respondinfo* info);
+    void initMessage(string encstr);
+    void initMessage(Respondinfo* info);
+    string encodeMsg();
+    void* decodeMsg();
+    ~RespondCodec();
+private:
+    string m_encStr;
+    RespondMsg m_msg;
+};
+
---
+++ ClientSecKey/SecKeyShm.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include "BaseShm.h"
+#include <string.h>
+#include "SecKeyNodeInfo.h"
+
+class SecKeyShm :
+	public BaseShm
+{
+public:
+
+	SecKeyShm(int key, int maxNode);
+	SecKeyShm(string pathname, int maxNode);
+	~SecKeyShm();
+
+	int shmWrite(SecKeyNodeInfo* pNodeInfo);
+	SecKeyNodeInfo shmRead(string clientID, string serverID);
+	SecKeyNodeInfo shmRead(int keyID);
+	SecKeyNodeInfo shmFirstNode();
+	void shmInit();
+
+private:
+
+	int m_maxNode;
+
+};
+
